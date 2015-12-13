88fbc7a3e057ec2bbeb3e8bf3a79d4ff0e88e6ce
Pavlo Medynskyy <medynskyypavlo@gmail.com>
2015-12-13 16:05:36 +0300

    added logger winston --> /libs/logger/log.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('./config/index');
+var log = require('./libs/logger/log')(module);
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
