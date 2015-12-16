089ce785faed00bfb82d0eb1232ee8f6a067371a
Pavlo Medynskyy <medynskyypavlo@gmail.com>
2015-12-17 02:16:38 +0300

    mongoose first steps

--- createDb.js
+++ createDb.js
@@ -1,42 +1,18 @@
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/test';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-
-  db.close();
+var schema = mongoose.Schema({
+	name: String
 });
 
-var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the document collection");
-    callback(result);
-  });
+schema.methods.meow = function() {
+	console.log('meow-meow...My name is ' + this.get('name'));
 }
 
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+var Cat = mongoose.model('Cat', schema); //cats
 
-// Connection URL
-var url = 'mongodb://localhost:27017/myproject';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
+var kitty = new Cat({ name: 'Zildjian'});
 
-  insertDocuments(db, function() {
-    db.close();
-  });
-});
+kitty.save(function(err, kitty, affected) {
+	kitty.meow();
+})
