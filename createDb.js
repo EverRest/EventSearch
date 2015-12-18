36ad8a9c27b0c9e5dd57ddc78819b3f952473368
Pavlo Medynskyy <medynskyypavlo@gmail.com>
2015-12-18 14:37:14 +0300

    added script for closing open connections in mongodb

--- createDb.js
+++ createDb.js
@@ -1,14 +1,24 @@
+var mongoose = require('./libs/mongoose');
 var User = require('./models/user').User;
 
-var user = new User({
-	username: "Tester2",
-	password: "secret"
-});
+//var user = new User({
+//	username: "Tester2",
+//	password: "secret"
+//});
 
-user.save(function(err, user, affected) {
-	if (err) throw err;
+//user.save(function(err, user, affected) {
+//	if (err) throw err;
 
-	User.findOne({username: "Tester"}, function(err, tester) {
-		console.log(tester);
-	})
+//	User.findOne({username: "Tester"}, function(err, tester) {
+//		console.log(tester);
+//	})
+//});
+
+mongoose.connection.on('open', function() {
+	var db = mongoose.connection.db;
+	db.dropDatabase(function(err) {
+		if (err) throw err;
+		console.log('OK');
+		mongoose.disconnect();
+	});
 });
