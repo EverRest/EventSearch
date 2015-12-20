8ded82d193541d990e96496d776608ddd6555eca
Pavlo Medynskyy <medynskyypavlo@gmail.com>
2015-12-20 14:23:15 +0200

    added carousel for how.html page + refactoring

--- public/js/common.js
+++ public/js/common.js
@@ -55,9 +55,32 @@ $(document).ready(function() {
 	$('nav .bottom_menu_btn').click(function(){
 		$('nav .bottom_menu').slideToggle().delay(700);
 	});		
-	//
+	//END
 
 	//FANCYBOX
 	$(".fancybox").fancybox();
-	//
+	//END
+
+	//CAROUSEL
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 1,
+		autoHeight : true
+	});
+	owl.on("mousewheel", ".owl-wrapper", function (e) {
+		if (e.deltaY > 0) {
+			owl.trigger("owl.prev");
+		} else {
+			owl.trigger("owl.next");
+		}
+		e.preventDefault();
+	});
+	$(".next_button").click(function() {
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function() {
+		owl.trigger("owl.prev");
+	});
+	//END
+
 });
